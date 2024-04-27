2bf6e4e37666811bb04aade39879e048b542ca74
Seeba184 <seenasimkani@gmail.com>
2024-04-27 13:28:11 -0400

    feat: calculate best day pair and return the value

--- stock_picker.rb
+++ stock_picker.rb
@@ -6,5 +6,8 @@ def stock_picker(arr)
       hash_of_day_pairs_and_profits.store([i, t], arr[t] - arr[i])
     end
   end
-  
+
+  best_days = hash_of_day_pairs_and_profits.key(hash_of_day_pairs_and_profits.values.max)
+
+  best_days
 end
