2346a9c2dc6efbb0ee0222ec858e222553df0d04
Seeba184 <seenasimkani@gmail.com>
2024-04-27 13:26:07 -0400

    feat: add for loops to append day pairs and profits to hash

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,3 +1,10 @@
 def stock_picker(arr)
   hash_of_day_pairs_and_profits = {}
+
+  for i in 0..(arr.length - 1)
+    for t in (i + 1)..(arr.length - 1)
+      hash_of_day_pairs_and_profits.store([i, t], arr[t] - arr[i])
+    end
+  end
+  
 end
